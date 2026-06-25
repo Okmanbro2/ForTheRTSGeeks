62a5c5d39e15b79b11f0b6c8e98c9d95051785f7
Okmanbro2 <103367850+Okmanbro2@users.noreply.github.com>
2026-06-25 19:55:57 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,38 +1,31 @@
 const http = require("http");
 const { Server } = require("socket.io");
-
 const PORT = process.env.PORT || 3000;
 
-// https
+// http
 const httpServer = http.createServer((req, res) => {
   res.writeHead(200);
   res.end("Game server is running!");
 });
-
 const io = new Server(httpServer, {
   cors: { origin: "*" }
 });
-
 const players = {};
-
 io.on("connection", (socket) => {
   console.log("Player connected:", socket.id);
-
   players[socket.id] = {
     x: 200 + Math.floor(Math.random() * 600),
     y: 200 + Math.floor(Math.random() * 600),
     id: socket.id,
     name: "Player"
   };
-
-  socket.on("join", (data) => {
+  socket.emit("currentPlayers", players);
+  socket.broadcast.emit("newPlayer", players[socket.id]);
+  socket.on("setName", (name) => {
     if (players[socket.id]) {
-      players[socket.id].name = data.name;
+      players[socket.id].name = name;
     }
-    socket.emit("currentPlayers", players);
-    socket.broadcast.emit("newPlayer", players[socket.id]);
   });
-
   socket.on("move", (data) => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
@@ -40,13 +33,11 @@ io.on("connection", (socket) => {
       socket.broadcast.emit("playerMoved", { id: socket.id, x: data.x, y: data.y });
     }
   });
-
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("playerLeft", socket.id);
   });
 });
-
 httpServer.listen(PORT, () => {
   console.log("Game server running on port " + PORT);
 });
